6203fad32467f05c46aab0e859be3e8ab890a2a2
SamPhillips12 <sam.phillips@mail.com>
2023-05-29 11:44:51 +0100

    first commit

---
+++ src/Components/Items/TodoItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../UI/Card";
+
+function TodoItem(props) {
+  return (
+    <Card>
+      <p>todo item</p>
+    </Card>
+  );
+}
+
+export default TodoItem;
